312ec9f81670b2b84279a8766566f0492736a886
Mahdi Lamb <mahdilamb@gmail.com>
2020-12-30 20:59:55 +0000

    correct cmocean using https://github.com/kthyng/cmocean-gmt bump version

--- src/main/java/net/mahdilamb/colormap/reference/sequential/Rain.java
+++ src/main/java/net/mahdilamb/colormap/reference/sequential/Rain.java
@@ -16,29 +16,30 @@ public final class Rain extends SequentialColormap {
      */
     public Rain() {
         super(
-                new Color(34, 27, 56),
-                new Color(34, 30, 58),
-                new Color(36, 41, 67),
-                new Color(37, 53, 76),
-                new Color(35, 64, 86),
-                new Color(30, 75, 95),
-                new Color(21, 87, 102),
-                new Color(9, 98, 107),
-                new Color(3, 109, 109),
-                new Color(9, 120, 110),
-                new Color(27, 130, 110),
-                new Color(52, 139, 109),
-                new Color(79, 147, 110),
-                new Color(103, 154, 114),
-                new Color(126, 161, 119),
-                new Color(147, 168, 126),
-                new Color(168, 174, 133),
-                new Color(188, 181, 141),
-                new Color(207, 188, 156),
-                new Color(216, 199, 180),
-                new Color(222, 212, 203),
+                new Color(238, 237, 243),
                 new Color(229, 225, 224),
-                new Color(238, 237, 243)
+                new Color(222, 212, 203),
+                new Color(216, 199, 180),
+                new Color(207, 188, 156),
+                new Color(188, 181, 141),
+                new Color(168, 174, 133),
+                new Color(147, 168, 126),
+                new Color(126, 161, 119),
+                new Color(103, 154, 114),
+                new Color(79, 147, 110),
+                new Color(52, 139, 109),
+                new Color(27, 130, 110),
+                new Color(9, 120, 110),
+                new Color(3, 109, 109),
+                new Color(9, 98, 107),
+                new Color(21, 87, 102),
+                new Color(30, 75, 95),
+                new Color(35, 64, 86),
+                new Color(37, 53, 76),
+                new Color(36, 41, 67),
+                new Color(34, 30, 58),
+                new Color(34, 27, 56)
+
 
         );
 
